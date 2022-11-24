45e7faf3ba0821d11e94008cf0a6b8cc4f79bfd7
kenjis <kenji.uui@gmail.com>
2022-11-24 11:18:13 +0900

    feat: show old input data when validation fails

--- app/Views/news/create.php
+++ app/Views/news/create.php
@@ -7,10 +7,12 @@
     <?= csrf_field() ?>
 
     <label for="title">Title</label>
-    <input type="input" name="title"><br>
+    <input type="input" name="title" value="<?= set_value('title') ?>">
+    <br>
 
     <label for="body">Text</label>
-    <textarea name="body" cols="45" rows="4"></textarea><br>
+    <textarea name="body" cols="45" rows="4"><?= set_value('body') ?></textarea>
+    <br>
 
     <input type="submit" name="submit" value="Create news item">
 </form>
